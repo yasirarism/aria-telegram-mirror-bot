7afc3daa3334b21abfa7571d55dd6e23d2ae2247
yasirarism <55983182+yasirarism@users.noreply.github.com>
2021-01-26 19:25:57 +0700

    Update and rename .constants.js.example to .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,15 +1,15 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
+  TOKEN: '1402710992:AAEGQW2gddGSlnQdTpuaP_BnbxmZjlFTOaw',
   ARIA_SECRET: 'myaria2',
   ARIA_DOWNLOAD_LOCATION: '/home/user/path/to/download/dir (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: 'id_of_Drive_folder_to_upload_into',
+  GDRIVE_PARENT_DIR_ID: '129T3ecpHaiQlNHJNstfb9KXdpF69qPZK',
   OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
-  SUDO_USERS: [012, 345],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [678, 901],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  SUDO_USERS: [617426792],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001255283935],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
@@ -22,11 +22,11 @@ module.exports = Object.freeze({
     path: '/botNotify'
   },
   COMMANDS_USE_BOT_NAME: {
-    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
+    ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
     NAME: "@nameOf_bot"
   },
   IS_TEAM_DRIVE: false,
   USE_SERVICE_ACCOUNT: false,
-  INDEX_DOMAIN: "",
-  TELEGRAPH_TOKEN: '' // Telegraph token, if you want to show search results in telegra.ph else keep it blank
+  INDEX_DOMAIN: "https://gd.yasir.my.id",
+  TELEGRAPH_TOKEN: 'edcac3589621e229c934e4ef6da45f55e6c95c874d2cbbe01883a1ad3c7c' // Telegraph token, if you want to show search results in telegra.ph else keep it blank
 });
